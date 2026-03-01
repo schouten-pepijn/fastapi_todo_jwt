e841f6d241268b3f79d502d3cc15c7b5e33c7a71
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-01 23:17:59 +0100

    feat: update datetime issue on User and Todo

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Column, DateTime
 from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING
@@ -11,7 +12,10 @@ class Todo(SQLModel, table=True):
     title: str
     description: str | None = None
     completed: bool = Field(default=False)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(DateTime(timezone=True), nullable=False),
+    )
 
     owner_id: int = Field(foreign_key="user.id", index=True, nullable=False)
     owner: "User" = Relationship(back_populates="todos")
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING, List
+from sqlalchemy import Column, DateTime
 from sqlmodel import Field, Relationship, SQLModel
 
 if TYPE_CHECKING:
     from app.models.todo import Todo
+    from app.models.refresh_session import RefreshSession
 
 
 class User(SQLModel, table=True):
@@ -11,6 +13,10 @@ class User(SQLModel, table=True):
     email: str = Field(index=True, unique=True, nullable=False)
     hashed_password: str = Field(nullable=False)
     is_active: bool = Field(default=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(DateTime(timezone=True), nullable=False),
+    )
 
+    refresh_sessions: List["RefreshSession"] = Relationship(back_populates="user")
     todos: List["Todo"] = Relationship(back_populates="owner")
